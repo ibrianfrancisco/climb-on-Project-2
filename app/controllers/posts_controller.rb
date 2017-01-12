a0186bc2402815d4269c78a7380cc8ad5fbca1f6
Brian Francisco <ibrianfrancisco@gmail.com>
2017-01-12 15:44:53 -0800

    filled in html and controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,48 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+  end
+
+ def new
+    @post = Post.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @user.posts.create(post_params)
+    redirect_to @user
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+    redirect_to user_path(@post.user_id)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :keyword)
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authorize, except: [:index, :show, :new]
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "You have signed up!"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :age, :bio, :email, :location, :password, :password_confirmation)
+  end
+end
