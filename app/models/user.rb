ed07f5dbe7ca313086a897b25521d79d65801916
Brian Francisco <ibrianfrancisco@gmail.com>
2017-01-16 23:09:37 -0800

    Added relationship model and has_manys

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,40 @@
 class User < ActiveRecord::Base
-  # after_initialize :set_defaults
   has_secure_password
   validates :email, presence: true, uniqueness: true
   has_many :posts, dependent: :destroy
-  has_many :comments
-  # What this means is , if I'm deleted, take out the parent as well so no errors are found.
+  has_many :comments, dependent: :destroy
+
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   has_attached_file :image, styles: { medium: "500x500>", thumb: "250x250>" }, presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   has_attached_file :bannerimg, styles: { medium: "500x500>", thumb: "250x250>" }, presence: true
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+   def following?(other_user)
+     relationships.find_by(followed_id: other_user.id)
+   end
+
+   def follow!(other_user)
+     relationships.create!(followed_id: other_user.id)
+   end
+
+   def unfollow!(other_user)
+     relationships.find_by(followed_id: other_user.id).destroy
+   end
+
+
+
+
   private
+
+
 
   # def set_defaults
   #   self.discipline = "General Science" if self.discipline.nil?
